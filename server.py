bada753df4c411898cf186392a145ffb4fc6561f
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-09-10 20:00:56 +0200

    Trying to run it as async.

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+import asyncio
 import uvicorn
 
-uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
+async def main():
+    config = uvicorn.Config("main:app", host="0.0.0.0", port=5000, log_level="info")
+    server = uvicorn.Server(config)
+    await server.serve()
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
