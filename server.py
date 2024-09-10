96ecfa0770c0c6f4aeccff565f8996c5b19cfd77
Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
2024-09-10 22:06:01 +0200

    Add debug.

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ try:
         uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
     proc = Process(target=main, args=(), daemon=True)
     proc.start()
+    print('Process launched')
     time.sleep(5)
 except Exception as e:
     print(e)
